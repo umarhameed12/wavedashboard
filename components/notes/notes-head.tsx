b76b5b7c4ac85000fd2370be6d7e1d52b1a10c89
Abdulrehman <140512505+umarhameed12@users.noreply.github.com>
2024-09-19 17:48:58 +0500

    update notes page

--- components/notes/notes-head.tsx
+++ components/notes/notes-head.tsx
@@ -3,53 +3,78 @@ import { Button } from "@material-tailwind/react";
 import Link from "next/link";
 import React from "react";
 
-const NotesHead = () => {
+interface NotesHead {
+  toggleDrawer: () => void;
+}
+
+const NotesHead = ({ toggleDrawer }: NotesHead) => {
   return (
     <div>
-      <div className="flex items-center justify-between">
-          <div>
-            <div className="flex gap-2 text-blue-600 text-sm font-medium">
-              <Link href="/dashboard">Home</Link>
-              <p>/</p>
-              <Link href="/dashboard/notes">Notes</Link>
-            </div>
-            <p className="text-2xl font-extrabold">Notes</p>
+      <div className="flex sm:items-center sm:justify-between sm:flex-row flex-col">
+        <div>
+          <div className="flex gap-2 text-blue-600 text-sm font-medium">
+            <Link href="/dashboard">Home</Link>
+            <p>/</p>
+            <Link href="/dashboard/notes">Notes</Link>
           </div>
-          <div className="flex items-center gap-3">
-            <Button className="border border-[#000000] border-opacity-25 p-2 hover:shadow-none shadow-none bg-transparent">
+          <p className="text-2xl font-extrabold">Notes</p>
+        </div>
+        <div className="flex items-center gap-3">
+          <div className="lg:hidden">
+            <Button
+              onClick={toggleDrawer}
+              className="bg-transparent shadow-none border dark:border-[#4f4f4f] border-[#000000] border-opacity-25 p-2"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
+                fill="none"
                 viewBox="0 0 24 24"
-                fill="currentColor"
-                className="h-5 w-5 text-[#000000] opacity-25"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6 w-5 h-5 text-gray-500"
               >
                 <path
-                  fillRule="evenodd"
-                  d="M6.97 2.47a.75.75 0 0 1 1.06 0l4.5 4.5a.75.75 0 0 1-1.06 1.06L8.25 4.81V16.5a.75.75 0 0 1-1.5 0V4.81L3.53 8.03a.75.75 0 0 1-1.06-1.06l4.5-4.5Zm9.53 4.28a.75.75 0 0 1 .75.75v11.69l3.22-3.22a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-4.5-4.5a.75.75 0 1 1 1.06-1.06l3.22 3.22V7.5a.75.75 0 0 1 .75-.75Z"
-                  clipRule="evenodd"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
                 />
               </svg>
             </Button>
-            <div className="flex items-center w-fit bg-gray-100 rounded-xl p-2 border border-[#000000] border-opacity-25">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="h-6 w-6 text-gray-500"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <input
-                type="text"
-                placeholder="Search Notes"
-                className="ml-2 w-full bg-transparent outline-none text-gray-700 placeholder-gray-500"
-              />
-            </div>
           </div>
+          <Button className="border border-[#000000] dark:border-[#4f4f4f] border-opacity-25 p-2 hover:shadow-none shadow-none bg-transparent">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="h-5 w-5 text-[#000000] dark:text-white opacity-25"
+            >
+              <path
+                fillRule="evenodd"
+                d="M6.97 2.47a.75.75 0 0 1 1.06 0l4.5 4.5a.75.75 0 0 1-1.06 1.06L8.25 4.81V16.5a.75.75 0 0 1-1.5 0V4.81L3.53 8.03a.75.75 0 0 1-1.06-1.06l4.5-4.5Zm9.53 4.28a.75.75 0 0 1 .75.75v11.69l3.22-3.22a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-4.5-4.5a.75.75 0 1 1 1.06-1.06l3.22 3.22V7.5a.75.75 0 0 1 .75-.75Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </Button>
+          <div className="flex items-center w-fit bg-gray-100 dark:bg-[#1e1e1e] rounded-xl p-2 border dark:border-[#4f4f4f] border-[#000000] border-opacity-25">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="h-6 w-6 text-gray-500"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <input
+              type="text"
+              placeholder="Search Notes"
+              className="ml-2 w-full bg-transparent outline-none text-gray-700 placeholder-gray-500"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- modules/notes/notes.tsx
+++ modules/notes/notes.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React, { useState, useEffect, useRef } from "react";
 import NotesHead from "@/components/notes/notes-head";
+import { Drawer, IconButton } from "@mui/material";
+import { Close } from "@mui/icons-material";
 
 type Tab =
   | "notes"
@@ -148,19 +150,31 @@ const Notes = () => {
     }
   };
 
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className="py-5 container m-auto max-w-[1250px] px-10">
-      <NotesHead />
-      <div className="flex h-screen bg-white rounded-xl mt-10">
+      <NotesHead toggleDrawer={toggleDrawer(true)} />
+      <div className="flex h-screen bg-white dark:bg-[#1e1e1e] rounded-xl mt-10">
         {/* Sidebar */}
-        <div className="w-1/4 p-4 bg-white shadow-md rounded-tl-xl rounded-bl-xl">
+        <div className="w-1/4 p-4 bg-white dark:bg-[#1e1e1e] rounded-tl-xl rounded-bl-xl lg:block hidden">
           <ul className="overflow-y-scroll no-scrollbar max-h-[60vh]">
             {defaultTabs.map((tab) => (
               <li
                 key={tab}
                 onClick={() => setActiveTab(tab)}
                 className={`p-3 mb-2 cursor-pointer rounded ${
-                  activeTab === tab ? "bg-gray-200" : "hover:bg-gray-100"
+                  activeTab === tab
+                    ? "bg-gray-200 dark:bg-[#ffffff] dark:bg-opacity-10"
+                    : "hover:bg-gray-100 dark:hover:bg-[#ffffff] dark:hover:bg-opacity-10"
                 }`}
               >
                 {tab.charAt(0).toUpperCase() + tab.slice(1)}
@@ -168,21 +182,59 @@ const Notes = () => {
             ))}
           </ul>
         </div>
+        <Drawer
+          open={open}
+          onClose={handleDrawerClose}
+        >
+          <div className="md:w-[500px] w-[300px] p-4 bg-white dark:bg-[#1e1e1e]  relative h-full">
+            <ul className="mt-10">
+              {defaultTabs.map((tab) => (
+                <li
+                  key={tab}
+                  onClick={() => setActiveTab(tab)}
+                  className={`p-3 mb-2 cursor-pointer rounded dark:text-white ${
+                    activeTab === tab
+                      ? "bg-gray-200 dark:bg-[#ffffff] dark:bg-opacity-10"
+                      : "hover:bg-gray-100 dark:hover:bg-[#ffffff] dark:hover:bg-opacity-10"
+                  }`}
+                >
+                  {tab.charAt(0).toUpperCase() + tab.slice(1)}
+                </li>
+              ))}
+            </ul>
+            <IconButton
+              onClick={handleDrawerClose}
+              sx={{
+                position: "absolute",
+                top: 4,
+                right: 8,
+                color: "black",
+                backgroundColor: "rgba(0, 0, 0, 0.4)",
+                "&:hover": {
+                  backgroundColor: "rgba(0, 0, 0, 0.6)",
+                },
+              }}
+              className="dark:text-white"
+            >
+              <Close />
+            </IconButton>
+          </div>
+        </Drawer>
 
         {/* Content Area */}
-        <div className="w-3/4 p-6 overflow-y-scroll no-scrollbar max-h-[100vh]">
-          <div className="bg-gray-100 rounded-xl border p-6">
+        <div className="lg:w-3/4 w-full p-6 overflow-y-scroll no-scrollbar max-h-[100vh]">
+          <div className="bg-gray-100 dark:bg-dark200 rounded-xl border dark:border-[#4f4f4f] p-6">
             {/* Add Note Input */}
             <div className="flex justify-center">
               {!isAddingNote ? (
                 <div
                   onClick={handleAddNote}
-                  className="bg-white p-2 mb-4 rounded-lg cursor-pointer shadow-md w-3/4"
+                  className="bg-white dark:bg-[#1e1e1e] p-2 mb-4 rounded-lg cursor-pointer shadow-md w-full sm:w-3/4"
                 >
                   Take a note...
                 </div>
               ) : (
-                <div className="bg-white  mb-4 rounded-xl shadow-md w-3/4">
+                <div className="bg-white dark:bg-[#1e1e1e] mb-4 rounded-xl shadow-md w-full sm:w-3/4">
                   {newNote.image && (
                     <div className="relative">
                       <img
@@ -236,10 +288,10 @@ const Notes = () => {
                       onChange={handleImageUpload}
                       className="mb-2 hidden"
                     />
-                    <div className="flex justify-between items-center">
+                    <div className="flex xss:justify-between xss:items-center xss:flex-row flex-col">
                       <button
                         onClick={triggerFileInput}
-                        className="p-2 bg-transparent text-black rounded hover:bg-gray-200"
+                        className="p-2 bg-transparent text-black dark:text-white rounded hover:bg-gray-200"
                       >
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
@@ -269,11 +321,11 @@ const Notes = () => {
             </div>
 
             {/* Card Content */}
-            <div className="grid grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
               {content[activeTab]?.map((item) => (
                 <div
                   key={item.id}
-                  className="bg-white shadow-md rounded-lg cursor-pointer h-fit"
+                  className="bg-white dark:bg-[#1e1e1e] shadow-md rounded-xl cursor-pointer h-fit"
                   onClick={() => handleEditNote(item)}
                 >
                   {item.image && (
@@ -307,79 +359,110 @@ const Notes = () => {
       {/* Edit Modal */}
       {isEditModalOpen && editingNote && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-          <div className="bg-white p-6 rounded-lg w-1/2">
-            <input
-              type="text"
-              value={editingNote.title}
-              onChange={(e) =>
-                setEditingNote({ ...editingNote, title: e.target.value })
-              }
-              className="w-full p-2 mb-2 border rounded"
-            />
-            <textarea
-              value={editingNote.description}
-              onChange={(e) =>
-                setEditingNote({ ...editingNote, description: e.target.value })
-              }
-              className="w-full p-2 mb-2 border rounded"
-            />
-            <input
-              type="file"
-              accept="image/*"
-              onChange={(e) => {
-                const file = e.target.files?.[0];
-                if (file) {
-                  const reader = new FileReader();
-                  reader.onloadend = () => {
-                    setEditingNote((prev) =>
-                      prev ? { ...prev, image: reader.result as string } : null
-                    );
-                  };
-                  reader.readAsDataURL(file);
-                }
-              }}
-              className="mb-2"
-            />
+          <div className="bg-white dark:bg-[#1e1e1e] rounded-lg w-1/4 overflow-y-scroll no-scrollbar max-h-[100vh]">
             {editingNote.image && (
               <img
                 src={editingNote.image}
                 alt="Note"
-                className="w-full h-64 object-cover mb-2"
+                className="w-full block object-cover mb-2"
               />
             )}
-            <div className="flex justify-between">
-              <button
-                onClick={handleSaveEdit}
-                className="px-4 py-2 bg-[#00A4EF] hover:bg-[#0078D7] text-white rounded"
-              >
-                Save Changes
-              </button>
-              <div className="flex gap-2">
-                <button
-                  onClick={handleDeleteNote}
-                  className="p-2 bg-transparent text-black rounded hover:bg-gray-200"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="h-6 w-6"
+            <div className="p-3">
+              <input
+                type="text"
+                value={editingNote.title}
+                placeholder="Title"
+                onChange={(e) =>
+                  setEditingNote({ ...editingNote, title: e.target.value })
+                }
+                className="w-full p-2 mb-2 outline-none text-lg font-bold"
+              />
+              <textarea
+                value={editingNote.description}
+                placeholder="Take a note..."
+                onChange={(e) =>
+                  setEditingNote({
+                    ...editingNote,
+                    description: e.target.value,
+                  })
+                }
+                className="w-full p-2 mb-2 outline-none text-sm"
+              />
+              <input
+                type="file"
+                accept="image/*"
+                ref={fileInputRef}
+                onChange={(e) => {
+                  const file = e.target.files?.[0];
+                  if (file) {
+                    const reader = new FileReader();
+                    reader.onloadend = () => {
+                      setEditingNote((prev) =>
+                        prev
+                          ? { ...prev, image: reader.result as string }
+                          : null
+                      );
+                    };
+                    reader.readAsDataURL(file);
+                  }
+                }}
+                className="mb-2 hidden"
+              />
+              <div className="flex justify-between items-center">
+                <div>
+                  <button
+                    onClick={triggerFileInput}
+                    className="p-2 bg-transparent text-black dark:text-white rounded hover:bg-gray-200"
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                    />
-                  </svg>
-                </button>
-                <button
-                  onClick={() => setIsEditModalOpen(false)}
-                  className="px-4 py-2 bg-transparent text-black rounded hover:bg-gray-300"
-                >
-                  Close
-                </button>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6 w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                      />
+                    </svg>
+                  </button>
+                </div>
+                <div className="flex gap-2">
+                  <button
+                    onClick={handleDeleteNote}
+                    className="p-2 bg-transparent text-black dark:text-white rounded hover:bg-gray-200"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="h-6 w-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                      />
+                    </svg>
+                  </button>
+                  <button
+                    onClick={() => setIsEditModalOpen(false)}
+                    className="px-4 py-2 bg-transparent text-black dark:text-white rounded hover:bg-gray-300"
+                  >
+                    Close
+                  </button>
+                  <button
+                    onClick={handleSaveEdit}
+                    className="px-4 py-2 bg-[#00A4EF] hover:bg-[#0078D7] text-white rounded"
+                  >
+                    Save Changes
+                  </button>
+                </div>
               </div>
             </div>
           </div>
